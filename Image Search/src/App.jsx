df07b1b9f93f0181a1ada423771c9c6b81d87946
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-11 20:42:33 +0530

    tittle and button

--- Image Search/src/App.jsx
+++ Image Search/src/App.jsx
@@ -1,13 +1,35 @@
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useRef } from 'react';
+import {Form} from 'react-bootstrap'
+
+const API_URL = 'https://api.unsplash.com/search/photos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(process.env.VITE_API_KEY)
+
+  const searchInput = useRef(null)
+
+  const handleSearch = (event) => {
+    event.preventDefault()
+  };
 
   return (
-    <>
-      
-    </>
-  )
+      <div className='container'>
+        <h1 className='title'>Image Search</h1>    
+        <div className='searchBar'>
+          <Form onSubmit={handleSearch}>
+            <Form.Control
+              type='search'
+              placeholder='Type Something'
+              ref={searchInput}
+            > 
+            </Form.Control>
+          </Form>
+          {/* <input type="search" placeholder='Type something to search...' className='search-input'/> */}
+        </div>
+
+      </div>
+  );
 }
 
 export default App
