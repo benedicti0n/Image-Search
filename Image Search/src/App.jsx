aa2eef029f1bac45fc44e54fa25c3a01284de177
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-12 16:20:01 +0530

    image fetching done

--- Image Search/src/App.jsx
+++ Image Search/src/App.jsx
@@ -1,16 +1,31 @@
+import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react'
 import {Form} from 'react-bootstrap'
 
 const API_URL = 'https://api.unsplash.com/search/photos'
+const IMAGES_PER_PAGE = 20
 
 function App() {
-  console.log(process.env.VITE_API_KEY)
-
   const searchInput = useRef(null)
+  const [images, setImages] = useState([]);
+  const [totalPages, setTotalPages] = useState(0)
+
+  const fetchImages = async () => {
+    try{
+      const { data } = await axios.get(`${API_URL}?query=${searchInput.current.value}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`
+      )
+      setImages(data.results)
+      setTotalPages(data.total_pages)
+
+    }catch(error){
+      console.log(error)
+    }
+  }
 
   const handleSearch = (event) => {
     event.preventDefault()
+    fetchImages()
   };
 
   return (
@@ -26,6 +41,20 @@ function App() {
             </Form.Control>
           </Form>
           {/* <input type="search" placeholder='Type something to search...' className='search-input'/> */}
+        </div>
+
+        <div className="images">
+          {
+            images.map((image) => {
+              return(
+                <img
+                id={image.id}
+                src={image.urls.small} 
+                alt={image.alt_description}
+                className='image'/>
+              )
+            })
+          }
         </div>
 
       </div>
